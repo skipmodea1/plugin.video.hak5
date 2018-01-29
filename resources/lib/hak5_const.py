141875a883fd355afaf52a9f908b1520a69d4da7
skipmodea1 <skipmodea1@outlook.com>
2018-01-29 17:35:49 +0100

    v1.0.4-SNAPSHOT (2018-01-29)
    - Fixed website url in addon.xml (thanks Dangelus!)

--- resources/lib/hak5_const.py
+++ resources/lib/hak5_const.py
@@ -22,8 +22,8 @@ TEKTHINGRECENTLYADDEDURL = 'http://www.hak5.org/category/episodes/tekthing/page/
 PINEAPPLEUNIVERSITYRECENTLYADDEDURL = 'http://www.hak5.org/category/episodes/pineapple-university'
 METASPLOITRECENTLYADDEDURL = 'http://www.hak5.org/category/episodes/metasploit-minute/page/001'
 HEADERS = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'}
-DATE = "2018-01-20"
-VERSION = "1.0.3"
+DATE = "2018-01-29"
+VERSION = "1.0.4-SNAPSHOT"
 
 if sys.version_info[0] > 2:
     unicode = str
